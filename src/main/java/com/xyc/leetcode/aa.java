becb000f06ff898780da9ebe8454b3036d342f05
xiaoyuchen1011 <1041734224@qq.com>
2020-05-23 11:30:38 +0800

    first commit

---
+++ src/main/java/com/xyc/leetcode/aa.java
@@ -0,0 +1,13 @@
+package com.xyc.leetcode;
+
+import java.util.ArrayList;
+
+public class aa {
+    public static void main(String[] args) {
+        int a =1,b=2;
+
+    }
+
+    public aa() {
+    }
+}
