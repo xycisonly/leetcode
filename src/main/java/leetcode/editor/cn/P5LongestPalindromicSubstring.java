57f28d1eb11638bb71966a528746bb245c0cc642
xiaoyuchen <1041734224@qq.com>
2022-05-26 14:14:51 +0800

    no message

--- src/main/java/leetcode/editor/cn/P5LongestPalindromicSubstring.java
+++ src/main/java/leetcode/editor/cn/P5LongestPalindromicSubstring.java
@@ -16,17 +16,15 @@
 
 package leetcode.editor.cn;
 
-import java.util.*;
-
 public class P5LongestPalindromicSubstring {
     public static void main(String[] args) {
         Solution solution = new P5LongestPalindromicSubstring().new Solution();
-        System.out.println(solution.longestPalindrome("babad"));
+        System.out.println(solution.longestPalindrome("1b1"));
     }
 
     //leetcode submit region begin(Prohibit modification and deletion)
     class Solution {
-        public String longestPalindrome(String s) {
+        public String longestPalindrome2(String s) {
             if (s == null || s.length() < 2) {
                 return s;
             }
@@ -55,11 +53,30 @@ public class P5LongestPalindromicSubstring {
             return s.substring(resultLeft, resultRight + 1);
         }
 
-        public String longestPalindrome1(String s) {
+        public String longestPalindrome(String s) {
             if (s.length() == 0) {
                 return "";
             }
+            int left = 0,right = 0;
+            for (int centerIndex = 0; centerIndex < s.length(); centerIndex++) {
+                int len1 = findLengthByCenter(centerIndex, centerIndex, s);
+                int len2 = findLengthByCenter(centerIndex, centerIndex + 1, s);
+                int len = Math.max(len1,len2);
+                if (len > right-left){
+                    //0 center-0 center+0 1 center-0 center+1 2 center-1 center+1 3 center-1 center+2
+                    left = centerIndex - len/2;
+                    right = centerIndex + (len+1)/2;
+                }
+            }
+            return s.substring(left,right+1);
+        }
 
+        private int findLengthByCenter(int left, int right, String s) {
+            while (left >= 0 && right < s.length()&&s.charAt(left) == s.charAt(right)) {
+                left--;
+                right++;
+            }
+            return right-left-2;
         }
 //        public String longestPalindrome(String s) {
 //            if (s.length() == 0) {
